7c25ca3795d26b45537f621b90255ff7298b09b5
icefire <erifeci@gmail.com>
2019-03-15 01:46:18 -0400

    Clean up chip manager pointer logic

--- ChipManager.cpp
+++ ChipManager.cpp
@@ -3,73 +3,52 @@
 
 namespace AutomataMod {
 
+static_assert(sizeof(ChipManager::ChipSlot) == 48, "ChipSlot isn't 48 bytes in size! This breaks pointer reading logic with game memory!");
+
 const int ChipManager::MAX_SLOT_COUNT = 300;
-const int ChipManager::SLOT_SIZE = 12;
 const uint32_t ChipManager::EMPTY_SLOT_ID = 0xFFFFFFFF;
 const uint32_t ChipManager::WAUP_CHIP_ID = 3007;
 const uint32_t ChipManager::RAUP_CHIP_ID = 3034;
 
-ChipManager::ChipSlot::ChipSlot(uint32_t* startPos) : 
-    unknown0(startPos), 
-    id(startPos + 1),
-    chipIndex(startPos + 2),
-    chipLevel(startPos + 3),
-    slotCost(startPos + 4),
-    setAPos(startPos + 5),
-    setBPos(startPos + 6),
-    setCPos(startPos + 7),
-    unknown1(startPos + 8),
-    unknown2(startPos + 9),
-    unknown3(startPos + 10),
-    unknown4(startPos + 11)
-{}
-
-void ChipManager::ChipSlot::reset()
+void ChipManager::reset(ChipSlot* slot)
 {
-    memset(unknown0, 0xFF, SLOT_SIZE * 4);
-    *unknown4 = 0;
+    if (!slot)
+        return;
+
+    memset(slot, 0xFF, sizeof(ChipSlot));
+    slot->unknown4 = 0;
 }
 
-ChipManager::ChipManager(uint32_t* chipTableRamStart, int32_t* money) : _chipTableRamStart(chipTableRamStart), _money(money)
+ChipManager::ChipManager(uint64_t chipTableRamStart, int32_t* money) : _firstChip(reinterpret_cast<ChipSlot*>(chipTableRamStart)), _money(money)
 {}
 
-ChipManager::ChipSlot ChipManager::getChipSlotBySlotIndex(int slotNumber)
+ChipManager::ChipSlot* ChipManager::getChipSlotById(uint32_t chipId)
 {
-    uint32_t* slotAddress = _chipTableRamStart + (slotNumber * SLOT_SIZE);
-    return ChipSlot(slotAddress);
-}
-
-int ChipManager::getChipSlotIndexById(uint32_t chipId)
-{
-    uint32_t iterId;
-    for (int i = 0; i < MAX_SLOT_COUNT; ++i) {
-        iterId = *(_chipTableRamStart + (i * SLOT_SIZE) + 1); // + 1 to skip the unknown field at beginning of each slot
-        if (iterId == chipId)
+    for (ChipSlot* i = _firstChip; i != _firstChip + MAX_SLOT_COUNT; ++i) {
+        if (i->id == chipId)
             return i;
     }
 
-    return -1;
+    return nullptr;
 }
 
 void ChipManager::addEndingEChips()
 {
-    int waupSlotIndex1 = -1;
-    int waupSlotIndex2 = -1;
-    int raupSlotIndex = -1;
+    ChipSlot* waupSlot1 = nullptr;
+    ChipSlot* waupSlot2 = nullptr;
+    ChipSlot* raupSlot = nullptr;
 
-    uint32_t chipId;
-    for (int i = 0; i < MAX_SLOT_COUNT; ++i) {
-        chipId = *(_chipTableRamStart + (i * SLOT_SIZE) + 1); // + 1 to skip the unknown field at beginning of each slot
-        if (chipId == WAUP_CHIP_ID) {
-            if (waupSlotIndex1 == -1)
-                waupSlotIndex1 = i;
-            else if (waupSlotIndex2 == -1)
-                waupSlotIndex2 = i;
-        } else if (chipId == RAUP_CHIP_ID && raupSlotIndex == -1) {
-            raupSlotIndex = i;
+    for (ChipSlot* i = _firstChip; i != _firstChip + MAX_SLOT_COUNT; ++i) {
+        if (i->id == WAUP_CHIP_ID) {
+            if (!waupSlot1)
+                waupSlot1 = i;
+            else if (!waupSlot2)
+                waupSlot2 = i;
+        } else if (i->id == RAUP_CHIP_ID && !raupSlot) {
+            raupSlot = i;
         }
 
-        if (raupSlotIndex > -1 && waupSlotIndex1 > -1 && waupSlotIndex2 > -1)
+        if (waupSlot1 && waupSlot2 && raupSlot)
             break;
     }
 
@@ -77,34 +56,29 @@ void ChipManager::addEndingEChips()
     if (*_money < 0)
         *_money = 0;
 
-    if (raupSlotIndex == -1 && (raupSlotIndex = getChipSlotIndexById(EMPTY_SLOT_ID)) != -1) {
-        ChipSlot slot = getChipSlotBySlotIndex(raupSlotIndex);
-        slot.reset();
-        *slot.unknown0 = 33;
-        *slot.id = RAUP_CHIP_ID;
-        *slot.chipIndex = 4;
-        *slot.chipLevel = 6;
-        *slot.slotCost = 28;
+    if (!raupSlot && (raupSlot = getChipSlotById(EMPTY_SLOT_ID)) != nullptr) {
+        reset(raupSlot);
+        raupSlot->unknown0 = 33;
+        raupSlot->id = RAUP_CHIP_ID;
+        raupSlot->chipIndex = 4;
+        raupSlot->chipLevel = 6;
+        raupSlot->slotCost = 28;
     }
 
-    if (waupSlotIndex1 == -1 && (waupSlotIndex1 = getChipSlotIndexById(EMPTY_SLOT_ID)) != -1) {
-        ChipSlot slot = getChipSlotBySlotIndex(waupSlotIndex1);
-        slot.reset();
-        *slot.unknown0 = 6;
-        *slot.id = WAUP_CHIP_ID;
-        *slot.chipIndex = 1;
-        *slot.chipLevel = 6;
-        *slot.slotCost = 28;
+    if (!waupSlot1 && (waupSlot1 = getChipSlotById(EMPTY_SLOT_ID)) != nullptr) {
+        reset(waupSlot1);
+        waupSlot1->unknown0 = 6;
+        waupSlot1->id = WAUP_CHIP_ID;
+        waupSlot1->chipLevel = 1;
+        waupSlot1->slotCost = 28;
     }
 
-    if (waupSlotIndex2 == -1 && (waupSlotIndex2 = getChipSlotIndexById(EMPTY_SLOT_ID)) != -1) {
-        ChipSlot slot = getChipSlotBySlotIndex(waupSlotIndex2);
-        slot.reset();
-        *slot.unknown0 = 6;
-        *slot.id = WAUP_CHIP_ID;
-        *slot.chipIndex = 1;
-        *slot.chipLevel = 6;
-        *slot.slotCost = 28;
+    if (!waupSlot2 && (waupSlot2 = getChipSlotById(EMPTY_SLOT_ID)) != nullptr) {
+        reset(waupSlot2);
+        waupSlot2->unknown0 = 6;
+        waupSlot2->id = WAUP_CHIP_ID;
+        waupSlot2->chipLevel = 1;
+        waupSlot2->slotCost = 28;
     }
 }
 
--- ChipManager.hpp
+++ ChipManager.hpp
@@ -6,48 +6,42 @@ namespace AutomataMod {
 
 class ChipManager
 {
-    uint32_t* _chipTableRamStart; // The memory address that the chip table starts at
+public:
+    struct ChipSlot
+    {
+        uint32_t unknown0; // starts at 0, changes based on chip in unknown ways...
+        uint32_t id; // starts at 3001, increments for every chip + all their supported levels
+
+        uint32_t chipIndex; // an index for each type of chip there is in the game (not including +1/2/3)
+        uint32_t chipLevel; // The chip boost level (+0, +1, +2, etc)
+        uint32_t slotCost; // Amount of slots the chip takes up in a set
+
+        // position of the chip in the chip sets. Set to 0xFFFFFFFF to not assign to set
+        uint32_t setAPos;
+        uint32_t setBPos;
+        uint32_t setCPos;
+
+        uint32_t unknown1; // Always 0xFFFFFFFF (except OS chip, which is 0)
+        uint32_t unknown2; // Always 0xFFFFFFFF (except OS chip, which is 0)
+        uint32_t unknown3; // Always 0xFFFFFFFF (except OS chip, which is 0)
+        uint32_t unknown4; // Always 0
+    };
+
+private:
+    ChipSlot* const _firstChip; // The memory address that the chip table starts at
     int32_t* _money;
 
     static const int MAX_SLOT_COUNT; // total amount of chips that can be stored on a player profile
-    static const int SLOT_SIZE; // size in DWORD's of each chip slot
 
+    // Empties the chip slot
+    void reset(ChipSlot* chip);
 public:
     static const uint32_t EMPTY_SLOT_ID; // chip ID indicating an empty chip slot
     static const uint32_t WAUP_CHIP_ID;
     static const uint32_t RAUP_CHIP_ID;
 
-    struct ChipSlot
-    {
-        uint32_t* unknown0; // starts at 0, changes based on chip in unknown ways...
-        uint32_t* id; // starts at 3001, increments for every chip + all their supported levels
-
-        uint32_t* chipIndex; // an index for each type of chip there is in the game (not including +1/2/3)
-        uint32_t* chipLevel; // The chip boost level (+0, +1, +2, etc)
-        uint32_t* slotCost; // Amount of slots the chip takes up in a set
-
-        // position of the chip in the chip sets. Set to 0xFFFFFFFF to not assign to set
-        uint32_t* setAPos;
-        uint32_t* setBPos;
-        uint32_t* setCPos;
-
-        uint32_t* unknown1; // Always 0xFFFFFFFF (except OS chip, which is 0)
-        uint32_t* unknown2; // Always 0xFFFFFFFF (except OS chip, which is 0)
-        uint32_t* unknown3; // Always 0xFFFFFFFF (except OS chip, which is 0)
-        uint32_t* unknown4; // Always 0
-
-        ChipSlot(uint32_t* startPos);
-
-        // Empties the chip slot
-        void reset();
-    };
-
-    ChipManager(uint32_t* chipTableRamStart, int32_t* money);
-    ChipSlot getChipSlotBySlotIndex(int index);
-
-    // returns a chip slop index containing the given chip ID, or -1 if not found
-    int getChipSlotIndexById(uint32_t chipId);
-
+    ChipManager(uint64_t chipTableRamStart, int32_t* money);
+    ChipSlot* getChipSlotById(uint32_t chipId);
     void addEndingEChips();
 };
 
