66ed38cea8ac63df7cbd5b8d206597e7e9339e6b
icefire <erifeci@gmail.com>
2019-08-31 14:54:10 -0400

    Remove savefile modification

--- AutomataMod.cpp
+++ AutomataMod.cpp
@@ -7,22 +7,17 @@
 
 namespace {
 
-const char16_t* VC3_NAME = u"vc3v1.3";
-const size_t VC3_NAME_LEN = std::char_traits<char16_t>::length(VC3_NAME);
-
 // Addresses are offsets in bytes relevant to processRamStart
 const uint64_t CURRENT_PHASE_ADDR = 0x1101D58;
 const uint64_t PLAYER_SET_NAME_ADDR = 0x147B4BC;
 const uint64_t IS_WORLD_LOADED_ADDR = 0x110ADC0;
 const uint64_t ITEM_TABLE_START_ADDR = 0x197C4C4;
-const uint64_t PLAYER_NAME_ADDR = 0x194BF88;
 
 char* currentPhase = nullptr;
 char16_t* playerName = nullptr;
 uint32_t* playerNameSet = nullptr;
 uint32_t* isWorldLoaded = nullptr;
 bool inventoryModded = false;
-bool nameModded = false;
 
 } // namespace
 
@@ -32,7 +27,6 @@ void checkStuff(uint64_t processRamStart)
 {
     AutomataMod::InventoryManager inventoryManager(processRamStart + ITEM_TABLE_START_ADDR);
     currentPhase = reinterpret_cast<char*>(processRamStart + CURRENT_PHASE_ADDR);
-    playerName = reinterpret_cast<char16_t*>(processRamStart + PLAYER_NAME_ADDR);
     playerNameSet = reinterpret_cast<uint32_t*>(processRamStart + PLAYER_SET_NAME_ADDR);
     isWorldLoaded = reinterpret_cast<uint32_t*>(processRamStart + IS_WORLD_LOADED_ADDR);
 
@@ -45,28 +39,12 @@ void checkStuff(uint64_t processRamStart)
             inventoryModded = true;
         }
 
-        if (!nameModded && *isWorldLoaded == 1 && *playerNameSet == 1) {
-            AutomataMod::log(LogLevel::LOG_INFO, "Detected we need to change savefile name. Changing.");
-            size_t nameLen = std::char_traits<char16_t>::length(playerName);
-            if (nameLen + VC3_NAME_LEN > 16) {
-                size_t offset = 16 - VC3_NAME_LEN;
-                memcpy(playerName + offset, VC3_NAME, VC3_NAME_LEN * sizeof(char16_t));
-            } else {
-                memcpy(playerName + nameLen, VC3_NAME, (VC3_NAME_LEN * sizeof(char16_t)) + 1); // +1 for terminating null
-            }
-
-            nameModded = true;
-        }
-
         if (*isWorldLoaded == 0 && *playerNameSet == 0) {
             if (inventoryModded) {
                 AutomataMod::log(LogLevel::LOG_INFO, "Detected the run has been reset. Resetting inventory checker.");
                 AutomataMod::log(LogLevel::LOG_INFO, "-------------------------------------------------------------------------------");
                 inventoryModded = false;
             }
-
-            if (nameModded)
-                nameModded = false;
         }
 
         std::this_thread::sleep_for(std::chrono::duration<double, std::milli>(250)); // check stuff 4 times a second
