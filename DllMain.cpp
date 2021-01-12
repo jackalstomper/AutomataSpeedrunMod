f0349cd4496c529cc55ac4272ab78054896f29ec
icefire <jackalstomper@users.noreply.github.com>
2021-01-12 18:10:07 -0500

    Set fast loads to all phase jumps

--- DllMain.cpp
+++ DllMain.cpp
@@ -1,6 +1,8 @@
 #include <windows.h>
 #include <thread>
 #include <d3d11.h>
+#include <stdexcept>
+#include <vector>
 #include "AutomataMod.hpp"
 #include "Log.hpp"
 #include "iat.hpp"
@@ -38,11 +40,56 @@ HRESULT WINAPI D3D11CreateDeviceHooked(IDXGIAdapter* pAdapter, D3D_DRIVER_TYPE D
             pFeatureLevels, FeatureLevels, SDKVersion, ppDevice, pFeatureLevel, ppImmediateContext);
 }
 
+void modPhaseJumpMethod(uint64_t processRamStartAddr) {
+    // Modify all calls to phasejump to use the fastload flag.
+    // Do this by inject a jump function to our custom code that sets the field to 0, then jumping back
+    std::vector<uint8_t> fastLoadCode = {
+        0x41, 0xC7, 0x01, 0x00, 0x00, 0x00, 0x00,       // mov [r9],0; This sets the fastload
+        0x53,                                           // push rbx; an instruction we overwrote to make the new jmp
+        0x56,                                           // push rsi; an instruction we overwrote to make the new jmp
+        0xEB, 0x08                                      // jmp NieRAutomata.exe+51A734; Go back to original code
+    };
+
+    std::vector<uint8_t> jumpCode = {
+        0xEB, 0xED // jmp NieRAutomata.exe+51A721
+    };
+
+    // replace two instructions starting at this location with a jmp to our code
+    // we'll repeat those instructions in our code
+    uint64_t codeChangeTargetLocation = processRamStartAddr + 0x51A732;
+
+    // A code cave between fuctions full of int 3(0xCC) bytes
+    uint64_t newCodeLocation = processRamStartAddr + 0x51A721;
+
+    void* newCodePointer = reinterpret_cast<void*>(newCodeLocation);
+
+    DWORD oldProtection;
+    if (VirtualProtect(newCodePointer, 19, PAGE_READWRITE, &oldProtection) == 0) {
+        AutomataMod::log(AutomataMod::LogLevel::LOG_ERROR, "Failed to fetch memory info for automata. We can't modify the phaseJump code.");
+        throw new std::runtime_error("Failed to fetch memory info for automata. We can't modify the phaseJump code.");
+    }
+
+    // write new code in the cave
+    memcpy(newCodePointer, fastLoadCode.data(), fastLoadCode.size());
+
+    // inject new jmp instruction into phasejump method to point to our new code
+    memcpy(reinterpret_cast<char*>(codeChangeTargetLocation), jumpCode.data(), jumpCode.size());
+
+    DWORD dummy;
+    if (VirtualProtect(newCodePointer, 19, oldProtection, &dummy) == 0) {
+        AutomataMod::log(AutomataMod::LogLevel::LOG_ERROR, "Failed to fetch memory info for automata. We can't modify the phaseJump code.");
+        throw new std::runtime_error("Failed to fetch memory info for automata. We can't modify the phaseJump code.");
+    }
+}
 
 void init() {
     AutomataMod::log(AutomataMod::LogLevel::LOG_INFO, "Initializing AutomataMod v1.7");
     uint64_t processRamStartAddr = reinterpret_cast<uint64_t>(GetModuleHandle(nullptr));
     AutomataMod::log(AutomataMod::LogLevel::LOG_INFO, "Process ram start: " + std::to_string(processRamStartAddr));
+
+    AutomataMod::log(AutomataMod::LogLevel::LOG_INFO, "Modifying phasejump function for faster loads");
+    modPhaseJumpMethod(processRamStartAddr);
+
     checkerThread = new std::thread([processRamStartAddr]() {
         AutomataMod::checkStuff(processRamStartAddr);
     });
